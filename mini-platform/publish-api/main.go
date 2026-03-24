6ba77cb460447bb19a699d02620412c0f50cc3ff
sir-onze <tiago96baptista@gmail.com>
2026-03-24 17:06:13 +0000

    initializing the grpc server only once

--- mini-platform/publish-api/main.go
+++ mini-platform/publish-api/main.go
@@ -16,26 +16,19 @@ type PublishRequest struct {
 	Data string `json:"data"`
 }
 
-func publishHandler(w http.ResponseWriter, r *http.Request) {
+type PublishApi struct {
+	storeServiceClient proto.StoreServiceClient
+}
+
+func (api *PublishApi) publishHandler(w http.ResponseWriter, r *http.Request) {
 	var req PublishRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
-	// create the grpc connection
-	conn, err := grpc.Dial(
-		"localhost:50051",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		http.Error(w, "could not connect to store", http.StatusInternalServerError)
-		return
-	}
-	defer conn.Close()
-	// create the grpc client
-	client := proto.NewStoreServiceClient(conn)
-	_, err = client.Store(r.Context(), &proto.StoreRequest{Id: req.ID, Data: req.Data})
+
+	_, err = api.storeServiceClient.Store(r.Context(), &proto.StoreRequest{Id: req.ID, Data: req.Data})
 	if err != nil {
 		http.Error(w, "could not store data", http.StatusInternalServerError)
 		return
@@ -46,9 +39,24 @@ func publishHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// create the grpc connection
+	conn, err := grpc.Dial(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal("could not connect to store", err)
+	}
+	defer conn.Close()
+	// create the grpc client
+	api := &PublishApi{
+		storeServiceClient: proto.NewStoreServiceClient(conn),
+	}
+
+	// Create the HTTP server
 	mux := http.NewServeMux()
-	mux.HandleFunc("/publish", publishHandler)
+	mux.HandleFunc("/publish", api.publishHandler)
 	log.Println("publish-api listening on :8080")
-	http.ListenAndServe(":8080", mux)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 }
